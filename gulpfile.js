81af5fb0476b3bc6f77729677c7ce66786463ccf
elghannay <mohamedelghannay@gmail.com>
2020-05-24 10:08:16 +0000

    setting up  gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars');
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
 
 
@@ -20,16 +21,16 @@ gulp.task('html', done => {
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssvars(), autoprefixer()]))
+        .pipe(postcss([nested(), cssvars(), autoprefixer()]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function () {
 
-    gulp.watch('./app/index.html', gulp.series('styles'));
+    gulp.watch('app/index.html', gulp.series('html'));
 
-    gulp.watch('./app/assets/styles/**/*.css', gulp.series('html'));
-
+    gulp.watch('app/assets/styles/**/*.css', gulp.series('styles'));
+    // trigger the task with "gulp watch"
 });
 
 
