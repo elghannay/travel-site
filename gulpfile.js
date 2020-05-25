47b9f7000140801dcc4815e5efb7c4ef186ddc73
elghannay <mohamedelghannay@gmail.com>
2020-05-25 03:31:40 +0000

    gulpfile configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,41 @@
-// Initialize modules
+/////////////////////////////////////////////
+/************ Initialize modules *********/
+/////////////////////////////////////////////
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    postcss_import = require('postcss-import');
+    postcss_import = require('postcss-import'),
+    browserSync = require('browser-sync').create();
+// we want to import only the create() method from that package.
 
+/////////////////////////////////////////////
+/************  function definitions *********/
+/////////////////////////////////////////////
 
+function reload(done) {
+    server.reload();
+    done();
+}
+
+// object for defining the source and destination folder
+const paths = {
+    scripts: {
+        src: 'src/scripts/*.js',
+        dest: 'dist/scripts/',
+    },
+    html: 'app/index.html',
+    css: {
+        src: './app/assets/styles/style.css',
+        dest: './app/temp/styles'
+    }
+};
+
+///////////////////////////////////////
+/************ creating tasks *********/
+///////////////////////////////////////
 
 
 gulp.task('default', function done() {
@@ -15,22 +43,27 @@ gulp.task('default', function done() {
     done();
 });
 
-gulp.task('html', done => {
-    console.log("Imagine something useful beingddddd done to your HTML here.");
-    done();
-});
-
 gulp.task('styles', function () {
-    return gulp.src('./app/assets/styles/style.css')
+    return gulp.src(paths.css.src)
+        // the order on the pipe does matter.
         .pipe(postcss([postcss_import(), cssvars(), nested(), autoprefixer()]))
-        .pipe(gulp.dest('./app/temp/styles'));
+        .pipe(gulp.dest(paths.css.dest));
 });
 
 gulp.task('watch', function () {
-
-    gulp.watch('app/index.html', gulp.series('html'));
-
-    gulp.watch('app/assets/styles/**/*.css', gulp.series('styles'));
+    // initializing the server!
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+    gulp.watch(paths.html).on('change', browserSync.reload);
+    // the line below cause watch to be stuck at reloading browser.
+    // gulp.watch('app/assets/styles/**/*.css', gulp.series('styles', browserSync.reload));
+    gulp.watch('app/assets/styles/**/*.css', gulp.series('styles', function (done) {
+        browserSync.reload();
+        done();
+    }));
     // trigger the task with "gulp watch"
 });
 
