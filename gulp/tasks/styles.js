1035dfd64a90ca7579e8e898102a86bb16cb0591
elghannay <mohamedelghannay@gmail.com>
2020-05-27 06:13:05 +0000

    hero responsive images

--- gulp/tasks/styles.js
+++
@@ -1,5 +0,0 @@
-gulp.task('styles', function () {
-    return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([postcss_import(), cssvars(), nested(), autoprefixer()]))
-        .pipe(gulp.dest('./app/temp/styles/')).pipe(browsersync.reload({ stream: true }));
-});
