7593e08758e69511dc256b1bc994c5dff7e5e198
elghannay <mohamedelghannay@gmail.com>
2020-06-02 15:42:16 +0100

    webpack config done

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,5 @@
 // when we include js with js we don't need to add .js
-var Person = require(".app/assets/scripts/modules/Person");
+var Person = require("./modules/Person");
 
 const person = new Person('mohamed', 'blue');
 person.greet();
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ const path= require('path');
 // loaders allow us to load all kind of code files like images css scss handlebars ...
 // look at the entry file
 module.exports = {
-    // entry: '.app/assets/scripts/app.js',
-    //     output: {
-    //         path: path.resolve(__dirname, './app/temp/scripts'),
-    //         filename: 'bundle.js',
-    // },
-        mode:'none',
+    entry: './app/assets/scripts/app.js',
+    // entry: path.resolve(__dirname, 'app') + '/assets/scripts/app.js',
+        output: {
+            path: path.resolve(__dirname, './app/temp/scripts'),
+            filename: 'bundle.js',
+    },
+    mode: 'development',
+    devtool: 'none',
+        
 }
